9e5c86b359be5aa1c21f553015a1a3eef1439bd5
seyed <armoon@yahoo.com>
2023-10-25 12:03:21 -0700

    text change

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script Name:                  HELLO WORLD
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      10/24/2023
+# Purpose:                      PRINT ON SCREEN
+# Declaration of variables
+# Declaration of functions
+# Main
+echo "Hello Hello Hello"
+
+# End
