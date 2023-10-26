ac9412e6b8c6ef74688706629c6192875356945b
seyed <armoon@yahoo.com>
2023-10-25 22:12:17 -0700

    function

---
+++ ops-challenge-3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script Name:                  VARIABLES 101
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      10/25/2023
+# Purpose:                      VARIABLES
+# Declaration of variables
+Apple_price=5
+files=`ls`
+Code_writer=`whoami`
+# Declaration of functions
+print_login(){
+    last 
+    echo "This is login history"
+}
+# Main
+# Calling the function
+
+print_login
+print_login
+print_login
+
+echo "The price of apple is: $Apple_price"
+echo "Hello"
+echo "This folder contains $files"
+echo "The person scripting is: $Code_writer"
+# End
