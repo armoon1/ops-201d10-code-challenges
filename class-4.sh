aae4efcd4eee3e4db2084e9074494f1ee934e557
seyed <armoon@yahoo.com>
2023-10-26 16:17:03 -0700

    dir

---
+++ class-4.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Script Name:                  Functions
+# Author:                       SEYED MEHDI HASHEMI SOHI
+# Date of latest revision:      10/26/2023
+# Purpose:                      VARIABLES
+
+
+# Creates four directories: dir1, dir2, dir3, dir4
+
+dir=(./dir1 ./dir2 ./dir3 ./dir4)
+
+
+# Put the names of the four directories in an array
+# References the array variable to create a new .txt file in each directory
+
+
+
+
+#List=(apple banana peach kiwi)
+# Declaration of functions
+
+
+#print_sum(){
+#    sum=$((A+B))
+ #   echo $sum
+#}
+
+# Main
+#print_sum
+mkdir $dir
+
+
+
+# Calling the function
+
+# End
