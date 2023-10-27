f60d4f81aad4ec98a70d0e1ec01a8ac37666a88f
seyed <armoon@yahoo.com>
2023-10-26 18:19:06 -0700

    challenge 4

--- class-4.sh
+++ class-4.sh
@@ -4,34 +4,28 @@
 # Author:                       SEYED MEHDI HASHEMI SOHI
 # Date of latest revision:      10/26/2023
 # Purpose:                      VARIABLES
-
-
 # Creates four directories: dir1, dir2, dir3, dir4
 
-dir=(./dir1 ./dir2 ./dir3 ./dir4)
+dir1="1.txt"
+dir2="2.txt"
+dir3="3.txt"
+dir4="4.txt"
 
 
+dir=("dir1" "dir2" "dir3" "dir4")
+files=($dir1 $dir2 $dir3 $dir4)
+#echo ${dir[*]}
 # Put the names of the four directories in an array
 # References the array variable to create a new .txt file in each directory
 
-
-
-
-#List=(apple banana peach kiwi)
-# Declaration of functions
-
-
-#print_sum(){
-#    sum=$((A+B))
- #   echo $sum
-#}
-
 # Main
-#print_sum
-mkdir $dir
+echo ${files[*]}
+mkdir ${dir[*]}
+
+for folder in "${dir[@]}"; do 
+    touch $folder"/"$dir1
+done
 
 
-
-# Calling the function
 
 # End
