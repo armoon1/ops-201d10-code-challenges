ec7ce5314ebee607eb933df37169289955e334e9
seyed <armoon@yahoo.com>
2023-10-29 22:07:46 -0700

    loop

--- ops5.sh
+++ ops5.sh
@@ -8,9 +8,23 @@
 
 # Declaration of array
 names=(a b c d e)
-
+count=4
 #For loop 
-for names in ${names[*]}
-do 
-    echo $names
+#for names in ${names[*]}
+#do 
+#    echo $names
+#done 
+#while loop
+
+
+#while [ $count -gt 0 ]; do 
+#    echo "Value of count is: $count"
+#    count=$(($count -1))
+#done 
+
+#echo "The condition no longer true."
+
+while true; do 
+    ps aux
+    read -p "ctrl+c  to exit"
 done 
