e0a77bf8c0248685eb000810086b2dc00260d21c
seyed <armoon@yahoo.com>
2023-10-31 18:33:19 -0700

    sysinfo

--- sys-info.sh
+++ sys-info.sh
@@ -12,11 +12,18 @@ lshw
 echo "assignment part 2"
 lshw | grep "bridge"
 #Add text to the output clearly indicating which component (such as CPU, RAM, etc.) the script is returning information about
-# CPU
-cpu_info=$(lshw -class processor | grep -E 'product:|vendor:|width:')
+# CPU Product, Vendor, Physical ID, Bus info, Width
+
+cpu_info=$(lshw -class processor | grep -E 'product:|vendor:|bus:|physical ID:|width:')
 
 # RAM
 ram_info=$(lshw -class memory | grep -E 'description:|size:|clock:')
+
+# GPU
+gpu_info=$(lshw -class graphic | grep -E 'description:|size:|clock:')
+
+# Nenwork
+network_info=$(lshw -class network | grep -E 'description:|size:|clock:')
 
 # CPU display
 echo "CPU Information:"
@@ -26,3 +33,11 @@ echo "$cpu_info"
 # RAM display
 echo "RAM Information:"
 echo "$ram_info"
+
+# GPU display
+echo "GPU Information:"
+echo "$gpu_info"
+
+# Network display
+echo "NETWORK Information:"
+echo "$network_info"
