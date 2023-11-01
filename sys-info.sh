9045924c611999828b61e55ab43628a5cc87b4b5
seyed <armoon@yahoo.com>
2023-10-31 17:55:49 -0700

    sysinfo

--- sys-info.sh
+++ sys-info.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Script Name:                  
+# Script Name:                  lswh
 # Author:                       SEYED MEHDI HASHEMI SOHI
 # Date of latest revision:      10/31/2023
-# Purpose:                      
+# Purpose:                      System info
 
 
 #search the output of lshw and return every line
@@ -12,17 +12,17 @@ lshw
 echo "assignment part 2"
 lshw | grep "bridge"
 #Add text to the output clearly indicating which component (such as CPU, RAM, etc.) the script is returning information about
-# Get information about the CPU
+# CPU
 cpu_info=$(lshw -class processor | grep -E 'product:|vendor:|width:')
 
-# Get information about RAM
+# RAM
 ram_info=$(lshw -class memory | grep -E 'description:|size:|clock:')
 
-# Display the CPU information with a label
+# CPU display
 echo "CPU Information:"
 echo "$cpu_info"
 
 
-# Display the RAM information with a label
+# RAM display
 echo "RAM Information:"
 echo "$ram_info"
