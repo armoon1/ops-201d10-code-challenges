c0da96f80594bed0be230b447388f819bb69d798
SEYED MEHDI HASHEMI SOHI <92489172+armoon1@users.noreply.github.com>
2023-11-07 22:24:33 -0800

    Update Ops_challenge_12.ps1

--- Ops_challenge_12.ps1
+++ Ops_challenge_12.ps1
@@ -1,1 +1,3 @@
-﻿ipconfig /all > C:\Users\SEYED\Desktop\network_report.txt
+ipconfig /all > C:\Users\SEYED\Desktop\network_report.txt
+Select-String -Path  C:\Users\SEYED\Desktop\network_report.txt -Pattern 'IPv4 Address'
+Remove-Item -Path C:\Users\SEYED\Desktop\network_report.txt
