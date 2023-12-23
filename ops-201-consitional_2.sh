fc484994a3812671d4f9f4c966e039b94e268929
armoon1 <armoon@yahoo.com>
2023-12-23 22:41:26 +0000

    organizing

--- ops-201-consitional_2.sh
+++ ops-201-consitional_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script Name:                  Conditionals
+# Script Name:                  Conditional
 # Author:                       SEYED MEHDI HASHEMI SOHI
 # Date of latest revision:      10/29/2023
 # Purpose:                      Loops
